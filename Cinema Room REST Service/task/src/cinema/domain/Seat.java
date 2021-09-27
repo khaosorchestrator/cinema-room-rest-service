d853bce4d70353202a6cc555117156fc2eda13dd
Lucas Almeida <developer.lucas24@gmail.com>
2021-09-27 23:24:48 +0100

    chore: add @lombok to reduce code boilerplate

--- Cinema Room REST Service/task/src/cinema/domain/Seat.java
+++ Cinema Room REST Service/task/src/cinema/domain/Seat.java
@@ -3,8 +3,12 @@ package cinema.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.*;
 
-
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Seat {
 
     @JsonIgnore
@@ -12,40 +16,13 @@ public class Seat {
 
     @JsonIgnore
     private boolean taken = false;
+
+    @NonNull
     private int row;
+    @NonNull
     private int column;
+    @NonNull
     private int price;
-
-    public Seat() {}
-    public Seat(int row, int column, int price) {
-        this.row = row;
-        this.column = column;
-        this.price = price;
-    }
-
-    public void setTaken(boolean taken) {
-        this.taken = taken;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public int getColumn() {
-        return column;
-    }
-
-    public int getRow() {
-        return row;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public boolean isTaken() {
-        return taken;
-    }
 
     @JsonIgnore
     public ObjectNode getPurchased() {
